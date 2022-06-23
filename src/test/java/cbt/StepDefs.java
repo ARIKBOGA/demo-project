0666355db4515a6c69272575e225c4dcf276f8c2
burakarikboga <barikboga42@gmail.com>
2022-06-23 21:32:15 +0300

    "@smoke" has been deleted

--- src/test/java/cbt/StepDefs.java
+++ src/test/java/cbt/StepDefs.java
@@ -1,7 +1,5 @@
 package cbt;
 
-import static org.junit.Assert.fail;
-
 import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Given;
@@ -12,7 +10,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -20,7 +17,7 @@ import java.util.concurrent.TimeUnit;
 public class StepDefs {
 
     @Given("^I am on the home page$")
-    public void i_am_on_the_home_page() throws Throwable {
+    public void i_am_on_the_home_page(){
         Driver.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         Driver.getDriver().manage().window().maximize();
         Driver.getDriver().get("http://etsy.com");
@@ -28,17 +25,17 @@ public class StepDefs {
     }
 
     @When("^I search for \"([^\"]*)\"$")
-    public void i_search_for(String search) throws Throwable {
+    public void i_search_for(String search)  {
         Driver.getDriver().findElement(By.cssSelector("[id*='search-query']")).sendKeys(search + Keys.ENTER);
     }
 
     @Then("^I should see the results$")
-    public void i_should_see_the_results() throws Throwable {
+    public void i_should_see_the_results() {
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search"));
     }
 
     @Then("^I should see more results$")
-    public void i_should_see_more_results() throws Throwable {
+    public void i_should_see_more_results()  {
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search"));
     }
 
